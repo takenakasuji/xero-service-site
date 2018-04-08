95948f54c8fe2b8f41e8b9d4f6546a407c94754e
takenakasuji <t.nksj.e.89@gmail.com>
2018-04-08 22:29:09 +0900

    feature: added new component and adjusted index.vue

---
+++ components/AppHeader.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="header">
+    <ul>
+      <li>aaaa</li>
+    </ul>
+  </div>
+</template>
+
+<style scoped>
+.header {
+  display: block;
+  height: 60px;
+  background-color: #333;
+}
+</style>
+
---
+++ components/KeyVisual.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="wrapper">
+    <ul>
+      <li>aaaa</li>
+    </ul>
+  </div>
+</template>
+
+<style scoped>
+.wrapper {
+  display: block;
+  height: 400px;
+  background-color: #0040FF;
+}
+</style>
