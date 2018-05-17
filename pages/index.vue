f6462ebd0459519705271633cbb24565e58f0276
takenakasuji <t.nksj.e.89@gmail.com>
2018-05-18 01:45:40 +0900

    added meta and title

--- pages/index.vue
+++ pages/index.vue
@@ -40,6 +40,13 @@ export default {
     CaseContent,
     ProfileContent,
     AppFooter
+  },
+  metaInfo: {
+    title: 'xero service site',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' }
+    ]
   }
 }
 </script>
